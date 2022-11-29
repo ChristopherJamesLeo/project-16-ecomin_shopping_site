5c1b43ed12367998db999db288d211b6d3bddc36
Christopher James Leo <111208339+ChristopherJamesLeo@users.noreply.github.com>
2022-11-29 12:02:53 +0630

    firstmainpage

--- js/home.js
+++ js/home.js
@@ -40,3 +40,46 @@ for (let i = 0; i < carouselIndi.length; i++) {
     })
     
 }
+
+let getEverydayCasualItemsContainer = document.querySelectorAll(".everyday-casual-items-container");
+console.log(getEverydayCasualItemsContainer);
+
+
+function showEverydayCasualItem(idex){
+    console.log(idex)
+    getEverydayCasualItemsContainer.forEach(function(containers){
+        containers.style.display = "none";
+    })
+    
+    getEverydayCasualItemsContainer[idex].style.display = "flex";
+}
+
+showEverydayCasualItem(0);
+
+let everydayCasulaItemsBtns = document.querySelectorAll(".exclusive-product-btn");
+
+function everyCasualLinkActive( ) {
+    everydayCasulaItemsBtns.forEach((item)=>
+    item.classList.remove("active"));
+    this.classList.add("active");
+    showEverydayCasualItem(idex = this.getAttribute("exc-show-btn"));
+};
+everydayCasulaItemsBtns.forEach( (item) =>item.addEventListener("click", everyCasualLinkActive));
+
+let getfashionItems = document.querySelectorAll(".fashion-for-you-items");
+let getfashionItem = document.querySelector(".fashion-for-you-items");
+let getFashionContainer = document.querySelector(".fashion-for-you-container");
+    // getFashionContainer.style.width = `${getfashionItem.offsetWidth*getfashionItems.length + 350 }px`;
+
+    
+
+let scrollXFun=(container,item,items,outWidth)=>{
+    container.style.width = `${item.offsetWidth*items.length + outWidth }px`;
+}
+scrollXFun(getFashionContainer,getfashionItem,getfashionItems,350)
+let getwho_wear_show_container = document.querySelector(".who_wear_show_container");
+let getwho_wear_show_imgs = document.querySelectorAll(".who_wear_show_img");
+console.log(getwho_wear_show_imgs); 
+let getwho_wear_show_img = document.querySelector(".who_wear_show_img");
+
+    scrollXFun(getwho_wear_show_container,getwho_wear_show_img,getwho_wear_show_imgs,0)
