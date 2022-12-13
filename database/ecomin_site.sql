16d3ed16eee55c2520d286413255bd0c05a14b38
Christopher James Leo <aungaungoo.hotmail@gmail.com>
2022-12-13 12:02:28 +0630

    createTables

--- database/ecomin_site.sql
+++ database/ecomin_site.sql
@@ -58,4 +58,90 @@ CREATE TABLE IF NOT EXISTS menproduct (
 );
 
 INSERT INTO menproduct (pm_code,title,price,size,category,stock,image,description) VALUES
-("pm-0001","shirt","45","md","man product","100","","sutible for summer");
+("pm-0001","shirt","45","md","man product","100","","sutible for summer"),
+("pm-0002","pin","23","lg","man product","150","","for winter");
+
+CREATE TABLE IF NOT EXISTS womenproduct (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pw_code VARCHAR(255) NOT NULL DEFAULT "Enter Product Code",
+    title VARCHAR(255) NOT NULL DEFAULT "Enter product title",
+    price VARCHAR(255) NOT NULL DEFAULT "Enter product price",
+    size CHAR(255) NOT NULL DEFAULT "Medium",
+    category VARCHAR(255) NOT NULL DEFAULT "Men Product",
+    stock MEDIUMINT NOT NULL DEFAULT 100,
+    image MEDIUMBLOB NULL,
+    description MEDIUMTEXT NOT NULL DEFAULT "Enter Product description"
+);
+
+INSERT INTO womenproduct (pw_code,title,price,size,category,stock,image,description) VALUES
+("pw-0001","t shirt","15","sm","women product","100","","for swimming"),
+("pw-0002","scart","10","md","women product","150","","for office");
+
+
+CREATE TABLE IF NOT EXISTS accessorites (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    pa_code VARCHAR(255) NOT NULL DEFAULT "Enter Product Code",
+    title VARCHAR(255) NOT NULL DEFAULT "Enter product title",
+    price VARCHAR(255) NOT NULL DEFAULT "Enter product price",
+    size CHAR(255) NOT NULL DEFAULT "Medium",
+    category VARCHAR(255) NOT NULL DEFAULT "Men Product",
+    stock MEDIUMINT NOT NULL DEFAULT 100,
+    image MEDIUMBLOB NULL,
+    description MEDIUMTEXT NOT NULL DEFAULT "Enter Product description"
+);
+
+INSERT INTO accessorites (pa_code,title,price,size,category,stock,image,description) VALUES
+("pa-0001","shoe","29","md","shoe","100","","for walk"),
+("pa-0002","wallet","50","sm","wallet","150","","for man");
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    p_code VARCHAR(255) NOT NULL DEFAULT "Enter Product Code",
+    title VARCHAR(255) NOT NULL DEFAULT "Enter product title",
+    price VARCHAR(255) NOT NULL DEFAULT "Enter product price",
+    size CHAR(255) NOT NULL DEFAULT "Medium",
+    category VARCHAR(255) NOT NULL DEFAULT "Men Product",
+    stock MEDIUMINT NOT NULL DEFAULT 100,
+    image MEDIUMBLOB NULL,
+    description MEDIUMTEXT NOT NULL DEFAULT "Enter Product description"
+);
+
+CREATE TABLE IF NOT EXISTS wishlists (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    p_code VARCHAR(255) NOT NULL DEFAULT "Enter Product Code",
+    title VARCHAR(255) NOT NULL DEFAULT "Enter product title",
+    price VARCHAR(255) NOT NULL DEFAULT "Enter product price",
+    size CHAR(255) NOT NULL DEFAULT "Medium",
+    category VARCHAR(255) NOT NULL DEFAULT "Men Product",
+    stock MEDIUMINT NOT NULL DEFAULT 100,
+    image MEDIUMBLOB NULL,
+    description MEDIUMTEXT NOT NULL DEFAULT "Enter Product description"
+);
+
+CREATE TABLE IF NOT EXISTS cart (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    p_code VARCHAR(255) NOT NULL DEFAULT "Enter Product Code",
+    title VARCHAR(255) NOT NULL DEFAULT "Enter product title",
+    price VARCHAR(255) NOT NULL DEFAULT "Enter product price",
+    size CHAR(255) NOT NULL DEFAULT "Medium",
+    category VARCHAR(255) NOT NULL DEFAULT "Men Product",
+    stock MEDIUMINT NOT NULL DEFAULT 100,
+    image MEDIUMBLOB NULL,
+    description MEDIUMTEXT NOT NULL DEFAULT "Enter Product description"
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message MEDIUMTEXT 
+);
+
+CREATE TABLE IF NOT EXISTS reviews (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    image MEDIUMBLOB NOT NULL,
+    comment MEDIUMTEXT 
+);
